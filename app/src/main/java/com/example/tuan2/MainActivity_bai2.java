54fefa0afeb63996a01f55e362a7e0c417174b23
GiangVo <giangvo0206@gmail.com>
2022-02-22 08:46:12 +0700

    Finish Lab02

---
+++ app/src/main/java/com/example/tuan2/MainActivity_bai2.java
@@ -0,0 +1,14 @@
+package com.example.tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_bai2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bai2);
+    }
+}
